6ad5b02c9e43138373048a3a5d12630dcf8008a6
slava hatnuke <slava.hatnuke@gmail.com>
2019-05-07 12:24:36 +0300

    - init

---
+++ README.md
@@ -0,0 +1,3 @@
+## leventext
+
+levenshtein text comparison
---
+++ index.js
@@ -0,0 +1,7 @@
+function LevenText({ngram = 5, separator = null} = {}) {
+    return function (string1, string2) {
+        return 0
+    };
+}
+
+module.exports = LevenText;
