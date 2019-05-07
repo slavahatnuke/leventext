429bcfe1aae4de427623056e48bce25bd5c0b35f
slava hatnuke <slava.hatnuke@gmail.com>
2019-05-07 12:53:49 +0300

    - hey zero version

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-function LevenText({ngram = 5, separator = null} = {}) {
+const {get} = require('fast-levenshtein');
+
+function wordSimilarity(string1, string2) {
+    const string1Size = `${string1}`.length;
+    const string2Size = `${string2}`.length;
+
+    const size = string1Size > string2Size ? string1Size : string2Size;
+
+    if (!size) {
+        return 1;
+    }
+
+    const distance = get(string1, string2);
+    // console.log({distance, size})
+
+    const similarity = (size - distance) / size;
+    return similarity < 0 ? 0 : similarity;
+}
+
+function Compare({ngram = 5, separator = null} = {}) {
     return function (string1, string2) {
-        return 0
+        return wordSimilarity(string1, string2);
     };
 }
 
-module.exports = LevenText;
+module.exports = Compare;
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,24 @@
-describe('index', () => {
-    it('ok', function () {
+const Compare = require('..');
 
+const expect = require('chai').expect;
+
+describe('index', () => {
+    it('different', function () {
+        const compare = Compare();
+        expect(compare('x', 'y')).equal(0)
+    });
+
+    it('equal', function () {
+        const compare = Compare();
+        expect(compare('x', 'x')).equal(1)
+    });
+
+    it('a bit different', function () {
+        const compare = Compare();
+        expect(+compare('hello world', 'hello men').toFixed(2)).equal(0.55)
+        expect(+compare('hello world', 'hello dude').toFixed(2)).equal(0.55)
+        expect(+compare('hello world', 'hello world here').toFixed(2)).equal(0.69)
+        expect(+compare('hello world', 'dude').toFixed(2)).equal(0)
+        expect(+compare('hello world', '').toFixed(2)).equal(0)
     });
 })
